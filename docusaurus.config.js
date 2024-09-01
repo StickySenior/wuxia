c6c07e182c7bc2f56b868a3d191e1186954e6217
StickySenior <aymen.ghaneme@gmail.com>
2024-09-01 19:19:03 +0200

    Added first version of Map

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'figures/land_wuxia_social_card.png',
       navbar: {
         title: 'Wuxia',
         logo: {
